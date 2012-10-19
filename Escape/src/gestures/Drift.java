022b66efa27db15b521057443c1bb270fb873751
kiuby <quentin.bernard@univ-paris-est.fr>
2012-10-19 20:10:27 +0200

    drift droit quasi ok!!!

--- Escape/src/gestures/Drift.java
+++ Escape/src/gestures/Drift.java
@@ -6,13 +6,28 @@ public class Drift implements Filter{
 	
 	@Override
 	public boolean checkGesture(List<Point> trace){
+		/*int bornes=25;*/
+		int size=trace.size(), nbPointsValid=0;
+		Point pBegin = trace.get(0);
+		Point pEnd = trace.get(size-1);
 		
 		
+		for(int i=0;i<size;i++){
+			Point pActual = trace.get(i);
+			if(pActual.getX()<pEnd.getX() && pActual.getX()>pBegin.getX())
+				if(pActual.getY()<pBegin.getY() && pActual.getY()>pEnd.getY())
+					nbPointsValid++;
+		}
+		System.out.println(nbPointsValid+" et "+trace.size());
+		if(nbPointsValid>=trace.size()*0.7)
+			return true;
+		return false;
+		/*
 		int x = trace.get(0).getX();
 		for(Point p : trace){
 			if(p.getX()<x-5)
 				return false;
 		}
-		return true;
+		return true;*/
 	}
 }
