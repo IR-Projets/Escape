75a6537743840cf329e88ca95edcf3aa3d8cdf9c
kiuby <quentin.bernard@univ-paris-est.fr>
2012-10-20 23:15:56 +0200

    maj variable local i+1 drift

--- Escape/src/gestures/Drift.java
+++ Escape/src/gestures/Drift.java
@@ -10,28 +10,21 @@ public class Drift implements Filter{
 		Objects.requireNonNull(trace);
 
 		int size=trace.size(), nbPointsValid=0;
-
 		for(int i=1;i<size-1;i++){
 			Point pLast = trace.get(i-1);
 			Point pActual = trace.get(i);
-			if(pActual.getX() < pLast.getX() && pActual.getX() > trace.get(i+1).getX()  ||   /*Left Drift*/
-					pActual.getX() > pLast.getX() && pActual.getX() < trace.get(i+1).getX()	)  /*Right Drift*/
-				if(pActual.getY() <= pLast.getY() && pActual.getY() >= trace.get(i+1).getY())
+			Point pNext = trace.get(i+1);
+			if(pActual.getX() < pLast.getX() && pActual.getX() > pNext.getX()  ||   /*Left Drift*/
+					pActual.getX() > pLast.getX() && pActual.getX() < pNext.getX()	)  /*Right Drift*/
+				if(pActual.getY() <= pLast.getY() && pActual.getY() >= pNext.getY())
 					nbPointsValid++;
 			//System.out.println("Point : ("+pActual.getX()+", "+pActual.getY());
 		}
 
-		System.out.println("Pts valide : "+nbPointsValid+ " sur "+ trace.size());
+		//System.out.println("Pts valide : "+nbPointsValid+ " sur "+ trace.size());
 		if(nbPointsValid>=trace.size()*0.75)
 			return true;
 		return false;
-		/*
-		int x = trace.get(0).getX();
-		for(Point p : trace){
-			if(p.getX()<x-5)
-				return false;
-		}
-		return true;*/
 	}
 
 
