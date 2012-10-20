01d5c11565f2abdc8d4ff671b45897b46f717b98
kiuby <quentin.bernard@univ-paris-est.fr>
2012-10-21 00:23:54 +0200

    maj Drift

--- Escape/src/gestures/Drift.java
+++ Escape/src/gestures/Drift.java
@@ -9,20 +9,36 @@ public class Drift implements Filter{
 	public boolean checkGesture(List<Point> trace){
 		Objects.requireNonNull(trace);
 
-		int size=trace.size(), nbPointsValid=0;
+		int size=trace.size(), nbLeft=0, nbRight=0;
+		int diffLimits=25;
+		double rate_error = (size>15)?0.75:0.5;/*Better rate of error if less points, because less precisions*/
+		
+		
 		for(int i=1;i<size-1;i++){
 			Point pLast = trace.get(i-1);
 			Point pActual = trace.get(i);
 			Point pNext = trace.get(i+1);
-			if(pActual.getX() < pLast.getX() && pActual.getX() > pNext.getX()  ||   /*Left Drift*/
-					pActual.getX() > pLast.getX() && pActual.getX() < pNext.getX()	)  /*Right Drift*/
+			if(pActual.getX() <= pLast.getX() && pActual.getX() >= pNext.getX())  /*Left Drift*/
 				if(pActual.getY() <= pLast.getY() && pActual.getY() >= pNext.getY())
-					nbPointsValid++;
+					nbLeft++;
+			if(	pActual.getX() >= pLast.getX() && pActual.getX() <= pNext.getX())  /*Right Drift*/
+				if(pActual.getY() <= pLast.getY() && pActual.getY() >= pNext.getY())
+					nbRight++;
+			/* Don't unified them them or we can have a curbic courbe that can be allow*/
+			
 			//System.out.println("Point : ("+pActual.getX()+", "+pActual.getY());
 		}
 
-		//System.out.println("Pts valide : "+nbPointsValid+ " sur "+ trace.size());
-		if(nbPointsValid>=trace.size()*0.75)
+		int difX = trace.get(0).getX()-trace.get(size-1).getX();
+		int difY = trace.get(0).getY()-trace.get(size-1).getY();
+		difX=(difX>0)?difX:-difX;
+		difY=(difY>0)?difY:-difY;
+		if(difX <diffLimits || difY < diffLimits)/* Don't allow vertical and horizontal movement */
+			return false;
+		
+		/*System.out.println("Pts valide left: "+nbLeft+ " sur "+ trace.size());
+		System.out.println("Pts valide right: "+nbRight+ " sur "+ trace.size());*/
+		if(nbLeft>=trace.size()*rate_error || nbRight>=trace.size()*rate_error)
 			return true;
 		return false;
 	}
