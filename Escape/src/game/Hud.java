853ef49e721c0589e6baeb30fe0fc3e959f7de20
kiuby <quentin.bernard@univ-paris-est.fr>
2012-10-31 03:43:18 +0100

    maj hud

--- Escape/src/game/Hud.java
+++ Escape/src/game/Hud.java
@@ -31,8 +31,8 @@ public class Hud implements LifeListener {
 	private final List<Weapon> weapons;
 	private int score;//Position of the life, because it's not a rectangle regular, so we have to update the points when losing life
 
-	private int sizeLife = 2*Variables.SCREEN_HEIGHT/25;// The total size for display the life
-	private final int echelle = sizeLife/Variables.MAX_LIFE +1;// The scale to compare one Point of life into a Percent of the Health Menu
+	private int sizeLife;// The total size for display the life
+	private final int echelle;// The scale to compare one Point of life into a Percent of the Health Menu
 	
 	private Hud(){
 		try {
@@ -43,7 +43,9 @@ public class Hud implements LifeListener {
 			System.exit(0);
 		}
 		score = 0;
-
+		sizeLife = hudLeft.getWidth()/2;
+		echelle = sizeLife/Variables.MAX_LIFE;
+		
 		weapons = new ArrayList<>();
 		//Factory???
 		weapons.add(new Fireball());
@@ -60,11 +62,12 @@ public class Hud implements LifeListener {
 			return;
 		int diffLife = oldLife-newLife;
 		sizeLife -= diffLife*echelle;
+		System.out.println("Sizelife"+sizeLife);
 	}
 
 	public void drawLife(Graphics2D graphics){
 		graphics.setColor(Variables.GREEN);
-		graphics.fillRect(Variables.SCREEN_WIDTH/6, 2*Variables.SCREEN_HEIGHT/25, sizeLife, Variables.SCREEN_HEIGHT/10 - Variables.SCREEN_HEIGHT/12);
+		graphics.fillRect(2*hudLeft.getWidth()/5, hudLeft.getHeight()/3, sizeLife, hudLeft.getHeight()/6);
 		//graphics.fillPolygon(posX,posY, 4);
 		score++;
 	}
@@ -79,10 +82,10 @@ public class Hud implements LifeListener {
 	//TODO
 	public void drawWeapons(Graphics2D graphics){
 		int xDeb =5*Variables.SCREEN_WIDTH/9, xEnd = Variables.SCREEN_WIDTH, nbWeapon = weapons.size();
-		int yDeb = Variables.SCREEN_WIDTH/22;
+		int yDeb = Variables.SCREEN_WIDTH/30;
 		int echelle = (xEnd-xDeb) / (5*nbWeapon), i=1;
 		
-		graphics.drawImage(hudRight, xDeb, 0, 4*Variables.SCREEN_WIDTH/9, Variables.SCREEN_HEIGHT/8, null);
+		graphics.drawImage(hudRight, xDeb, 0, hudRight.getWidth(), hudRight.getHeight(), null);
 		Iterator<Weapon> it = weapons.iterator();
 
 		while(it.hasNext()){
@@ -97,7 +100,7 @@ public class Hud implements LifeListener {
 		drawLife(graphics);
 				
 		//Draw the HUD
-		graphics.drawImage(hudLeft, 0, 0, 2*Variables.SCREEN_WIDTH/5, Variables.SCREEN_HEIGHT/5, null);
+		graphics.drawImage(hudLeft, 0, 0, hudLeft.getWidth(), hudLeft.getHeight(), null);
 
 		//Draw Weapons
 		drawWeapons(graphics);
