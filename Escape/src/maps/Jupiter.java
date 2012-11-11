83f80642622d3c1f8b7c449130ffcd910304e472
ludo6577 <ludo6577@gmail.com>
2012-11-11 17:23:55 +0100

    Changement de map

--- Escape/src/maps/Jupiter.java
+++ Escape/src/maps/Jupiter.java
@@ -1,15 +1,22 @@
 package maps;
 
 import game.Environnement;
+import game.Ressources;
 
 import java.io.IOException;
 
 
 public class Jupiter extends Map{
 
-	public Jupiter() throws IOException {
-		super();
+	public Jupiter() {
+		super(Ressources.getImage("images/maps/jupiter.png"), 0.2f);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void computeMap() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- Escape/src/maps/Moon.java
+++ Escape/src/maps/Moon.java
@@ -1,15 +1,23 @@
 package maps;
 
 import game.Environnement;
+import game.Ressources;
 
 import java.io.IOException;
 
 
 public class Moon extends Map{
 
-	public Moon() throws IOException {
-		super();
+	public Moon() {
+		super(Ressources.getImage("images/maps/moon.png"), 0.2f);
 		// TODO Auto-generated constructor stub
+	}
+
+	
+	@Override
+	public void computeMap() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
