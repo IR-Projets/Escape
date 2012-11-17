3c01bd4e279292fbea79f8082cb73e811c06d2f2
kiuby <quentin.bernard@univ-paris-est.fr>
2012-11-17 23:49:34 +0100

    dernier commit...

---
+++ BERNARDQuentinFELTZLudovic_EscapeIR/src/game/Variables.java
@@ -0,0 +1,99 @@
+package game;
+
+import java.awt.Color;
+
+
+/**
+ * This class represents all our Global Variables, used during the game.
+ * 
+ * @author Quentin Bernard et Ludovic Feltz
+ */
+
+
+/* <This program is an Shoot Them up space game, called Escape-IR, made by IR students.>
+ *  Copyright (C) <2012>  <BERNARD Quentin & FELTZ Ludovic>
+
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+public class Variables {
+
+	/**
+	 * Debug mode.
+	 */
+	public final static boolean DEBUG = false;
+	
+	/*
+	 * Application variables
+	 */
+	public static final String APPLICATION_NAME = "Escape IR";
+	public static final int SCREEN_WIDTH = 500;
+	public static final int SCREEN_HEIGHT = 700;
+	public static final String IMAGES_URL = "images/";
+	
+	/*
+	 * Render variables
+	 */
+	public static final int TICKS_PER_SECOND = 25;
+	public static final int SKIP_TICKS = 350 / TICKS_PER_SECOND;
+	public static final int MAX_FRAMESKIP = 20;
+	public static final int LOOP_SKIP = 60;
+	
+	/*
+	 * Some static colors
+	 */
+	public final static Color GREEN = new Color(0,125,0);
+	public final static Color RED = new Color(255,0,0);
+	public final static Color BLUE = new Color(0,0,255);
+	public final static Color WHITE = new Color(255,255,255);
+	public final static Color BLACK = new Color(0,0,0);
+	public final static Color ORANGE = new Color(255,165,0);
+	
+	/*
+	 * Main ship
+	 */
+	public final static int SHIP_VELOCITY = 1000;/* Speed of the main ship */
+	public final static int SHIP_BULLET_VELOCITY = 1000;/* Speed of the main ship */
+	public final static float SHIP_DAMPING = 0.4f;
+	public final static int SHIP_LIFE = 100;
+	
+	/*
+	 * Gesture variables
+	 */
+	public final static int TRACE_DELETE_RATE = 5;/*If we increase the value, the delete time of Trace become lower*/
+	public final static double TRACE_VARIATION_MAX = 40;/* The limit intervalle to accept movement -> Angle Bornes */
+	public static final int TRACE_LENGTH_MIN = 4;
+	
+	/*
+	 * World variables
+	 */
+	public static final float WORLD_GRAVITY_X = 0;
+	public static final float WORLD_GRAVITY_Y = 0;
+
+	public static final float WORLD_TIME_STEP = 1.0f / 60.f;
+	public static final int WORLD_VELOCITY_ITERATION = 10;
+	public static final int WORLD_POSITION_ITERATION = 8;
+
+	public static final float WORLD_BORDER = 50;
+	
+	/*
+	 * Test
+	 */
+	public static final int TEST_LAG = 35;
+	
+	
+	
+	
+}
